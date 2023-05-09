cba0f2d0cdbf656023aac4f05dbf9e9d836f54f0
danilorare <danilo@rareid.com.au>
2023-05-09 13:31:42 +1000

    Added ACF to the emission section

--- template-parts/ca-circles.php
+++ template-parts/ca-circles.php
@@ -1,23 +1,30 @@
 <div class="panel">
     <div class="wrap">
         <div class="grid">
-            <div class="col-1-4 dm-column-circle">
-                <div class="dm-circle-container">
-                    <div class="dm-center-circle">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/circle-icon-1.png">
-                        <p>32.8%</p>
-                    </div>
+
+        <?php if( have_rows('emissions_container') ){
+            while( have_rows('emissions_container') ){
+                the_row();
+                $icon_emissions = get_sub_field('icon_emissions');
+                $emissions_percentage = get_sub_field('emissions_percentage');
+                $emissions_title = get_sub_field('emissions_title');
+        ?>
+
+        <div class="col-1-4 dm-column-circle">
+            <div class="dm-circle-container">
+                <div class="dm-center-circle">
+                    <img src="<?php echo $icon_emissions;  ?>">
+                    <p><?php echo $emissions_percentage;  ?></p>
                 </div>
-                <p class="dm-sub">Electricity</p>
             </div>
-            <div class="col-1-4 dm-column-circle">
-                <div class="dm-circle-container">t</div>
-            </div>
-            <div class="col-1-4 dm-column-circle">
-                <img src="<?php echo get_template_directory_uri(); ?>/images/full-circle-1.png">
-            </div>
-            <div class="col-1-4 dm-column-circle">t</div>
-            <div class="col-1-4 dm-column-circle">t</div>
+            <p class="dm-sub"><?php echo $emissions_title;  ?></p>
+        </div>
+
+        <?php
+            }
+        } ?>
+            
+            
         </div>
     </div>
 </div>
