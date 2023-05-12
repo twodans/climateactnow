9242b5c7db2ac73ccb7dcda3a452ca5412bc1e4f
danilorare <danilo@rareid.com.au>
2023-05-12 14:04:50 +1000

    Updated slider images

--- template-parts/ca-performance.php
+++ template-parts/ca-performance.php
@@ -3,37 +3,43 @@
 <div class="panel" id="basic-waypoint">
 
         <div class="grid dm-perfomance">
-            <div class="col-1-2 dm-perfomance-left">
-            <div class="match">
-                <h2>Let’s turn Australia from a laggard to a leader in climate action</h2>
-                <p class="dm-text-perfomance">Australia is ranked in the bottom 10 countries for climate action – a lowly 55 out of 63 nations on the <br><a href="https://ccpi.org/" target="_blank">Climate Change Performance Index</a></p>
+            <div class="col-1-2 dm-perfomance-left hidde-400">
+                <div class="match">
+                    <h2>Let’s turn Australia from a laggard to a leader in climate action</h2>
+                    <p class="dm-text-perfomance">Australia is ranked in the bottom 10 countries for climate action – a lowly 55 out of 63 nations on the <br><a href="https://ccpi.org/" target="_blank">Climate Change Performance Index</a></p>
+                </div>
+
+                 <div class="dm-white-box holder" >
+                    <div class="triangle"><img src="<?php bloginfo('template_url'); ?>/images/slide-indicator.svg" alt=">"></div>
+                    <div class="dm-blur-top"></div>
+                        <ul class="dm-list" >    
+                            <li class=""><span class="lgtblue">45.</span> Czech Republic <span class="dm-number">44.16</span></li>
+                            <li class=""><span class="lgtblue">46.</span> Belarus <span class="dm-number">43.69</span></li>
+                            <li class=""><span class="lgtblue">47.</span> Türkiye <span class="dm-number">43.32</span></li>
+                            <li class=""><span class="lgtblue">48.</span> Algeria <span class="dm-number">42.26</span></li>
+                            <li class=""><span class="lgtblue">49.</span> Argentina <span class="dm-number">41.19</span></li>
+                            <li class=""><span class="lgtblue">50.</span> Japan <span class="dm-number">40.85</span></li>
+                            <li class=""><span class="lgtblue">51.</span> China <span class="dm-number">38.80</span></li>
+                            <li class=""><span class="lgtblue">52.</span> United States <span class="dm-number">38.53</span></li>
+                            <li class=""><span class="lgtblue">53.</span> Hungary <span class="dm-number">38.51</span></li>
+                            <li class=""><span class="lgtblue">54.</span> Poland <span class="dm-number">37.94</span></li>
+                            <li class=""><span class="lgtblue">55.</span> Australia <span class="dm-number">36.26</span></li>
+                            <li class=""><span class="lgtblue">56.</span> Malaysia <span class="dm-number">33.51</span></li>
+                            <li class=""><span class="lgtblue">57.</span> Chinese Taipei <span class="dm-number">28.35</span></li>
+                        </ul>
+                    <div class="dm-blur-bottom"></div>
+
+                </div>
             </div>
 
-            <div class="dm-white-box holder" >
-            <div class="triangle"><img src="<?php bloginfo('template_url'); ?>/images/slide-indicator.svg" alt=">"></div>
-            <div class="dm-blur-top"></div>
-                    <ul class="dm-list" >    
-                        <li class=""><span class="lgtblue">45.</span> Czech Republic <span class="dm-number">44.16</span></li>
-                        <li class=""><span class="lgtblue">46.</span> Belarus <span class="dm-number">43.69</span></li>
-                        <li class=""><span class="lgtblue">47.</span> Türkiye <span class="dm-number">43.32</span></li>
-                        <li class=""><span class="lgtblue">48.</span> Algeria <span class="dm-number">42.26</span></li>
-                        <li class=""><span class="lgtblue">49.</span> Argentina <span class="dm-number">41.19</span></li>
-                        <li class=""><span class="lgtblue">50.</span> Japan <span class="dm-number">40.85</span></li>
-                        <li class=""><span class="lgtblue">51.</span> China <span class="dm-number">38.80</span></li>
-                        <li class=""><span class="lgtblue">52.</span> United States <span class="dm-number">38.53</span></li>
-                        <li class=""><span class="lgtblue">53.</span> Hungary <span class="dm-number">38.51</span></li>
-                        <li class=""><span class="lgtblue">54.</span> Poland <span class="dm-number">37.94</span></li>
-                        <li class=""><span class="lgtblue">55.</span> Australia <span class="dm-number">36.26</span></li>
-                        <li class=""><span class="lgtblue">56.</span> Malaysia <span class="dm-number">33.51</span></li>
-                        <li class=""><span class="lgtblue">57.</span> Chinese Taipei <span class="dm-number">28.35</span></li>
-                    </ul>
-                <div class="dm-blur-bottom"></div>
+            <div class="col-1-2 dm-perfomance-left show-400">
+                <div class="match">
+                    <h2>Let’s turn Australia from a laggard to a leader in climate action</h2>
+                    <p class="dm-text-perfomance">Australia is ranked in the bottom 10 countries for climate action – a lowly 55 out of 63 nations on the <br><a href="https://ccpi.org/" target="_blank">Climate Change Performance Index</a></p>
+                </div>
+                <img src="<?php bloginfo('template_url'); ?>/images/slider-mobile.svg" alt="Countries">
+            </div>
 
-
- </div>
-
-
-</div>
             <div class="col-1-2 dm-perfomance-right hidde-400">
                 <div class="match">
                     <h2>‘75 by 35’ is realistic and <br class="hidden-640">achievable for Australia</h2>
@@ -47,6 +53,10 @@
             </div>
 
             <div class="col-1-2 dm-perfomance-right show-400">
+                <div class="match">
+                    <h2>‘75 by 35’ is realistic and <br class="hidden-640">achievable for Australia</h2>
+                    <p class="dm-text-perfomance">We need to meet the ambition of our global peers. Three states are leading the way. We need all states to follow.</p>
+                </div>
                 <img src="<?php bloginfo('template_url'); ?>/images/slider-flags.svg" alt="Perfomance">
             </div>
 
